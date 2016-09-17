2d67cfe13db657af28661821b5c395d21f4cd7d6
tmftake <tmftake@gmail.com>
2016-09-17 23:47:15 +0900

    test2

---
+++ otameshi/src/otameshi/Otameshi.java
@@ -0,0 +1,12 @@
+package otameshi;
+
+public class Otameshi {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Git.");
+    }
+
+}
