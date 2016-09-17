c2398c278a382a722e06767fcf8a98f6ebb4f522
tmftake <tmftake@gmail.com>
2016-09-17 23:52:25 +0900

    dev1

--- otameshi/src/otameshi/Otameshi.java
+++ otameshi/src/otameshi/Otameshi.java
@@ -6,7 +6,7 @@ public class Otameshi {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Hello Git.");
+        System.out.println("Hello Git-dev.");
     }
 
 }
